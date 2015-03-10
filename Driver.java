318b1220e39fbf11d6c2e73ce348a4e1fc7b9308
Katie Knutson <katieknutson@Katies-MacBook-Pro.local>
2015-03-10 08:04:11 -0500

    update

--- Driver.java
+++ Driver.java
@@ -1,4 +1,4 @@
-import java.io.PrintWriter
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
--- ResponseThread.java
+++ ResponseThread.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class ResponseThread extends Thread
 {	
 	private Socket s;
-	private String hello = "<html><body><h1>Hello World</h1><img src=spongeBob.jpg/></body></html>";
-	private String index = "<html><body><h1>Homepage</h1></body></html>";
-	private String error = "<html><body><h1>404: Page Not Found</h1></body></html>";
 	
 	public ResponseThread(Socket s)
 	{
@@ -19,33 +16,11 @@ public class ResponseThread extends Thread
 		try
 		{
 			Scanner input = new Scanner(this.s.getInputStream());
-			String req = input.nextLine();
-			System.out.println(req);
-			String[] parts = req.split(" ");
-			String theFunction = parts[0];
-			String thePage = parts[1];
-			String theProtocol = parts[2];
-			System.out.println(thePage);
-			
-			PrintWriter output = new PrintWriter(this.s.getOutputStream());
-			if(thePage.equalsIgnoreCase("/index.html") || thePage.equalsIgnoreCase("/"))
+			PrintWriter output = new PrintWriter(this.s.getOutputStream(), true);
+			while(true)
 			{
-				output.println(index);
-			}
-			else if(thePage.equalsIgnoreCase("/hello.html"))
-			{
-				output.println(error);
-			}
-			else
-			{
-				output.println(error);
-			}
-			output.flush();
-			output.close();
-			
-			for(int i = 0; i < 10000000; i++)
-			{
-				System.out.println("Wasting Time");
+				String req = input.nextLine();
+				output.println(req);
 			}
 		}
 		catch(Exception e)
